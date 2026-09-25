796f13f15f705d93316ab21e0f3140a7b97a9372
Matheus Godoy Dev <matheusgodoy.dev@gmail.com>
2026-09-25 00:03:29 -0400

    Foi criado o teste unitário para o exercicio 1035

---
+++ ExerciciosLogicaDeProgramacao.TestesUnitarios/Exercicios/Exercicio1035Test.cs
@@ -0,0 +1,47 @@
+﻿using ExerciciosLogicaDeProgramacao.Exercicios;
+using FluentAssertions;
+
+namespace ExerciciosLogicaDeProgramacao.TestesUnitarios.Exercicios
+{
+    [TestClass]
+    public class Exercicio1035Test
+    {
+        private Exercicio1035 _exercicio1035;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _exercicio1035 = new Exercicio1035();
+        }
+
+        [TestMethod]
+        public void AoProcessarDeveRetonarMensagemDeValoresNaoAceitos()
+        {
+            int numA = 5;
+            int numB = 6;
+            int numC = 7;
+            int numD = 8;
+
+            var resultado = _exercicio1035.Processar(numA, numB, numC, numD);
+
+            resultado
+                .Should()
+                .Be("Valores não aceitos");
+        }
+
+        [TestMethod]
+        public void AoProcessarDeveRetonarMensagemDeValoresAceitos()
+        {
+            int numA = 2;
+            int numB = 3;
+            int numC = 2;
+            int numD = 6;
+
+            var resultado = _exercicio1035.Processar(numA, numB, numC, numD);
+
+            resultado
+                .Should()
+                .Be("Valores aceitos");
+        }
+    }
+}
