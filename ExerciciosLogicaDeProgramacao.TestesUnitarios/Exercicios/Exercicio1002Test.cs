758fa5c1953fa193f8d02ed3caa69b2f9baf5252
Matheus Godoy Dev <matheusgodoy.dev@gmail.com>
2026-09-25 00:03:12 -0400

    Foi criado o teste unitário para o exercicio 1002

---
+++ ExerciciosLogicaDeProgramacao.TestesUnitarios/Exercicios/Exercicio1002Test.cs
@@ -0,0 +1,29 @@
+﻿using ExerciciosLogicaDeProgramacao.Exercicios;
+using FluentAssertions;
+
+namespace ExerciciosLogicaDeProgramacao.TestesUnitarios.Exercicios
+{
+    [TestClass]
+    public class Exercicio1002Test
+    {
+        private Exercicio1002 _exercicio1002;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _exercicio1002 = new Exercicio1002();
+        }
+
+        [TestMethod]
+        public void AoProcessarDeveRetornarValorDaAreaDoCirculo()
+        {
+            double raio = 100.64;
+
+            var resultado = _exercicio1002.Processar(raio);
+
+            resultado
+                .Should()
+                .Be("A=31819,3103");
+        }
+    }
+}
