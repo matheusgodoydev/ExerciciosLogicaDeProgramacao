5945e9d7aa643c7cd1fa364fbe4dab3ad3dc98d4
Matheus Godoy Dev <matheusgodoy.dev@gmail.com>
2026-09-25 00:03:03 -0400

    Foi criado o teste unitário para o exercicio 1001

---
+++ ExerciciosLogicaDeProgramacao.TestesUnitarios/Exercicios/Exercicio1001Test.cs
@@ -0,0 +1,30 @@
+﻿using ExerciciosLogicaDeProgramacao.Exercicios;
+using FluentAssertions;
+
+namespace ExerciciosLogicaDeProgramacao.TestesUnitarios
+{
+    [TestClass]
+    public class Exercicio1001Test
+    {
+        private Exercicio1001 _exercicio1001;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _exercicio1001 = new Exercicio1001();
+        }
+
+        [TestMethod]
+        public void AoProcessarDeveRetornarASomaDosDoisNumeros()
+        {
+            var numA = 10;
+            var numB = 5;
+
+            var resultado = _exercicio1001.Processar(numA, numB);
+
+            resultado
+                .Should()
+                .Be("X = 15");
+        }
+    }
+}
