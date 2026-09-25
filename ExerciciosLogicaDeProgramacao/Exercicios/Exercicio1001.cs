6742cf59303139e410f46cdbe06919364e54188f
Matheus Godoy Dev <matheusgodoy.dev@gmail.com>
2026-09-24 23:59:35 -0400

    Foi feito o exercicio 1001

---
+++ ExerciciosLogicaDeProgramacao/Exercicios/Exercicio1001.cs
@@ -0,0 +1,27 @@
+﻿namespace ExerciciosLogicaDeProgramacao.Exercicios
+{
+    public class Exercicio1001
+    {
+        /* Extremamente básico - 1001
+        
+        Leia duas variáveis, chamadas A e B , e some-as, atribuindo o resultado à variável X. 
+        Imprima X como mostrado abaixo. 
+        Imprima a linha final após o resultado; caso contrário, você receberá um erro de apresentação.
+
+        Entrada: 10, 9
+        Saída: "X = 19"
+
+        Entrada: -10, 4
+        Saída: "X = -6"  
+        
+        Entrada: 15, -7
+        Saída: "X = 8"
+
+        */
+
+        public string Processar(int numA, int numB)
+        {
+            return $"X = {numA + numB}";
+        }
+    }
+}
